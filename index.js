ff34db88f7cbdcf34459cb16f1cf1f87b85fc412
zhangguo <zhangguo@ulearning.cn>
2023-04-23 19:50:43 +0800

    d

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const app = express()
 
+// 静态文件
+app.use(express.static('public'))
+
 // 接口
 app.get('/api/hello', (req, res) => {
   res.send('Hello World!')
 })
-
-// 静态文件
-app.use(express.static('public'))
 
 // 启动服务器
 app.listen(process.env.PORT || 3000, () => {
