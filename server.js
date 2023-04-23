28e4a9d6ad9eb792d714076dcf0d778e14e5cda7
zhangguo <zhangguo@ulearning.cn>
2023-04-23 15:44:12 +0800

    temp commit

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const http = require('http');
 
-const hostname = '0.0.0.0';
-const port = process.env.PORT || 3000;
-
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, world!');
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('Hello World!');
+  res.end();
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(process.env.PORT || 3000, () => {
+  console.log('Server listening on port %d', server.address().port);
 });
